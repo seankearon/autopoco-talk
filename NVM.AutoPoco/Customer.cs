c9e986bab9836e16141dd455466b65bcb5461377
seankearon <sean@tackaberry.org>
2015-04-22 17:49:08 +0100

    4 - Conventions

--- NVM.AutoPoco/Customer.cs
+++ NVM.AutoPoco/Customer.cs
@@ -29,7 +29,8 @@ namespace NVM.AutoPoco
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string Address { get; set; }
+        public string ShippingAddress { get; set; }
+        public string BillingAddress { get; set; }
         public string EmailAddress { get; set; }
         public string Telephone { get; set; }
 

0e1a32b8cfa28962491d5e3d980f1b7f1027b6d1
seankearon <sean@tackaberry.org>
2015-04-22 17:37:07 +0100

    2 - With AutoPoco.

--- NVM.AutoPoco/TestData.cs
+++ NVM.AutoPoco/TestData.cs
@@ -1,26 +1,56 @@
-﻿namespace NVM.AutoPoco
+﻿using System.Linq;
+using AutoPoco;
+using AutoPoco.DataSources;
+using AutoPoco.Engine;
+
+namespace NVM.AutoPoco
 {
     public static class TestData
     {
-        public static Customer[] Items 
+        static TestData()
+        {
+            var factory = AutoPocoContainer.Configure(x =>
+            {
+                x.Conventions(c =>
+                {
+                    c.UseDefaultConventions();
+                });
+
+                x.Include<Customer>()
+                    .Setup(c => c.Id).Use<IntegerIdSource>()
+                    .Setup(c => c.FirstName).Use<FirstNameSource>()
+                    .Setup(c => c.LastName).Use<LastNameSource>()
+                    ;
+
+            });
+
+            Session = factory.CreateSession();
+        }
+
+        public static IGenerationSession Session { get; set; }
+
+        public static Customer[] Items
         {
             get
             {
-                return new[]
-                {
-                    new Customer
-                    {
-                        Id = 1,
-                        FirstName = "Sean",
-                        LastName = "Kearon"
-                    },
-                    new Customer
-                    {
-                        Id = 2,
-                        FirstName = "Gillian",
-                        LastName = "Macdonald"
-                    }
-                };
+                return Session.List<Customer>(2).Get().ToArray();
+                /*
+                                return new[]
+                                {
+                                    new Customer
+                                    {
+                                        Id = 1,
+                                        FirstName = "Sean",
+                                        LastName = "Kearon"
+                                    },
+                                    new Customer
+                                    {
+                                        Id = 2,
+                                        FirstName = "Gillian",
+                                        LastName = "Macdonald"
+                                    }
+                                };
+                */
             }
         }
     }
